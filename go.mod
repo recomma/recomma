1d7be71f5093a1856aa1ab3368d18c1c8958fafc
Yorick Terweijden <hi@terwey.me>
2025-08-06 03:06:29 +0200

    initial

---
+++ go.mod
@@ -0,0 +1,44 @@
+module github.com/terwey/recomma
+
+go 1.24.5
+
+require (
+	github.com/dgraph-io/badger/v4 v4.8.0
+	github.com/terwey/3commas-sdk-go v0.0.0
+	golang.org/x/sync v0.16.0
+	golang.org/x/time v0.12.0
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-openapi/errors v0.22.0 // indirect
+	github.com/go-openapi/strfmt v0.23.0 // indirect
+	github.com/google/flatbuffers v25.2.10+incompatible // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
+	github.com/jedib0t/go-pretty/v6 v6.6.8 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oapi-codegen/nullable v1.1.0 // indirect
+	github.com/oapi-codegen/runtime v1.1.2 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	go.mongodb.org/mongo-driver v1.14.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
+)
+
+replace github.com/terwey/3commas-sdk-go => ../3commas-sdk-go
---
+++ main.go
@@ -0,0 +1,343 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/terwey/3commas-sdk-go/threecommas"
+	"golang.org/x/sync/semaphore"
+
+	badger "github.com/dgraph-io/badger/v4"
+)
+
+type marketOrdersCache struct {
+	Data map[metadata]threecommas.MarketOrder
+	mu   sync.RWMutex
+}
+
+func NewMarketOrdersCache() *marketOrdersCache {
+	return &marketOrdersCache{
+		Data: make(map[metadata]threecommas.MarketOrder),
+	}
+}
+
+func (c *marketOrdersCache) Add(md metadata, order threecommas.MarketOrder) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.Data[md] = order
+}
+
+func main() {
+	// ordersMap := make(map[metadata]threecommas.MarketOrder)
+	moc := NewMarketOrdersCache()
+	sem := semaphore.NewWeighted(25)
+
+	db, err := badger.Open(badger.DefaultOptions("badger"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	client, err := threecommas.New3CommasClient(config)
+	if err != nil {
+		log.Fatalf("Could not create client: %s", err)
+	}
+
+	listBotsOpts := []threecommas.ListBotsParamsOption{
+		threecommas.WithScopeForListBots(threecommas.Enabled),
+	}
+
+	// let's load the current month
+	prefix := []byte(strconv.Itoa(quarterOfYear(time.Now())))
+	log.Printf("prefix: %s\n", prefix)
+	seen, err := loadSeenKeys(db, prefix)
+	if err != nil {
+		log.Fatalf("cannot get seen hashes: %s", err)
+	}
+
+	bots, err := client.ListBots(context.Background(), listBotsOpts...)
+	if err != nil {
+		apiErr := &threecommas.APIError{}
+		if errors.As(err, &apiErr) {
+			log.Fatalf("Could not list bots: %s\n%s", err, apiErr.ErrorPayload)
+		} else {
+			log.Fatalf("Could not list bots: %s", err)
+		}
+	}
+
+	var wg sync.WaitGroup
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{
+		"ID",
+		"Name",
+		"Trades",
+		"PnL",
+		"Avg. daily PnL",
+		"Exchange",
+		"Pair",
+		"Status",
+	})
+
+	for _, bot := range bots {
+		t.AppendRow(table.Row{
+			bot.Id,
+			*bot.Name,
+			bot.FinishedDealsCount,
+			bot.FinishedDealsProfitUsd,
+			"",              // ?? no idea which
+			bot.AccountName, // query Account info to get the Exchange info
+			bot.Pairs,
+			fmt.Sprintf("%d / %d", bot.ActiveDealsCount, bot.MaxActiveDeals),
+			bot.IsEnabled,
+		})
+	}
+	t.Render()
+
+	for _, bot := range bots {
+		deals, err := client.GetListOfDeals(context.Background(), threecommas.WithBotIdForListDeals(bot.Id))
+
+		if err != nil {
+			log.Fatalf("Could not list deals for bot %d: %s", bot.Id, err)
+		}
+
+		t := table.NewWriter()
+		t.SetOutputMirror(os.Stdout)
+		t.AppendHeader(table.Row{
+			"Deals for Bot",
+			bot.Id,
+		})
+		t.AppendSeparator()
+		t.AppendHeader(table.Row{
+			"ID", "Pair", "Created", "Completed S.O.", "Active S.O.", "Max S.O.", "Profit %", "Profit (amt)", "Bought Vol.", "Bought Amt.", "Status",
+		})
+
+		for _, d := range deals {
+			actualProfit, err := d.ActualProfit.Get()
+			if err != nil {
+				log.Printf("actual profit was not set: %s", err)
+			}
+			t.AppendRow(table.Row{
+				d.Id,
+				d.Pair, // e.g. "DOGE/USDT"
+				d.CreatedAt.Format("2006-01-02 15:04:05"), // your preferred layout
+				d.CompletedSafetyOrdersCount,
+				d.CurrentActiveSafetyOrdersCount,
+				d.MaxSafetyOrders,
+				d.ActualProfitPercentage, // e.g. "+1.07%"
+				actualProfit,             // e.g. "+$0.16"
+				d.BoughtVolume,           // e.g. "16.58921 USDT"
+				d.BoughtAmount,           // e.g. "80.000000 DOGE"
+				d.Status,                 // e.g. "active"
+			})
+		}
+
+		t.Render()
+
+		for _, deal := range deals {
+
+			wg.Add(1)
+			go func() {
+				if err := sem.Acquire(context.Background(), 1); err != nil {
+					log.Printf("semaphore acquire failed: %v", err)
+					return
+				}
+				defer sem.Release(1)
+				defer wg.Done()
+				orders, err := client.GetMarketOrdersForDeal(context.Background(), deal.Id)
+
+				if err != nil {
+					log.Fatalf("Could not list market orders for deal %d: %s", deal.Id, err)
+				}
+
+				for _, order := range orders {
+					md := metadata{
+						BotID:     deal.BotId,
+						DealID:    deal.Id,
+						OrderID:   order.OrderId,
+						CreatedAt: order.CreatedAt,
+					}
+
+					moc.Add(md, order)
+				}
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	// we now have all unprocessed MarketOrders
+
+	// unprocessed := unprocessedMarketOrders(db, md.BotId, md.DealID, orders)
+	// unprocessed := unprocessedMarketOrdersMap(db, moc.Data)
+
+	log.Printf("Seen %d: %v", len(seen), seen)
+
+	t = table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Key", "DealID", "ID", "Side", "Order Type", "Status", "Rate", "Amount", "Volume", "Created", "Updated"})
+
+	for md, order := range moc.Data {
+		_, ok := seen[md.String()]
+		if ok {
+			continue
+		}
+
+		// might be older than the batch
+		if seenKey(db, []byte(md.String())) {
+			continue
+		}
+
+		// key := fmt.Sprintf("%d::%d::%s", deal.BotId, deal.Id, order.OrderId)
+		err := db.Update(func(txn *badger.Txn) error {
+			data, err := json.Marshal(order)
+			if err != nil {
+				return err
+			}
+
+			e := badger.NewEntry([]byte(md.String()), data)
+			return txn.SetEntry(e)
+		})
+		if err != nil {
+			log.Fatalf("Could not add MarketOrder to DB: %s", err)
+		}
+
+		t.AppendRow(table.Row{
+			md.String(),
+			md.DealID,
+			order.OrderId,
+			order.OrderType,
+			order.DealOrderType,
+			order.StatusString,
+			order.Rate,
+			order.Quantity,
+			order.Total,
+			order.CreatedAt,
+			order.UpdatedAt,
+		})
+	}
+
+	t.Render()
+
+	log.Printf("MarketOrders %d", len(moc.Data))
+}
+
+// 1 → Jan–Mar, 2 → Apr–Jun, 3 → Jul–Sep, 4 → Oct–Dec
+func quarterOfYear(t time.Time) int {
+	return (int(t.Month())-1)/3 + 1
+}
+
+func loadSeenKeys(db *badger.DB, prefix []byte) (map[string]struct{}, error) {
+	seen := make(map[string]struct{})
+
+	err := db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		opts.Prefix = prefix
+
+		it := txn.NewIterator(opts)
+		defer it.Close()
+		for it.Rewind(); it.Valid(); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			seen[string(key)] = struct{}{}
+		}
+		return nil
+	})
+	return seen, err
+}
+
+func seenKey(db *badger.DB, key []byte) bool {
+	err := db.View(func(txn *badger.Txn) error {
+		log.Printf("Looking for %s\n", key)
+		_, err := txn.Get(key)
+		return err
+	})
+
+	switch err {
+	case badger.ErrKeyNotFound:
+		// unprocessed = append(unprocessed, order)
+		log.Printf("Unprocessed %s\n", key)
+		return false
+
+	case nil:
+		return true
+
+	default:
+		log.Printf("Unexpected error for key %s: %s", key, err)
+	}
+
+	return false
+}
+
+type metadata struct {
+	BotID     int
+	DealID    int
+	OrderID   string
+	CreatedAt time.Time
+}
+
+func (md *metadata) String() string {
+	return fmt.Sprintf("%d::%s::%d::%d::%s", quarterOfYear(md.CreatedAt), md.CreatedAt.Format("2006-01-02"), md.BotID, md.DealID, md.OrderID)
+}
+
+func unprocessedMarketOrdersMap(db *badger.DB, orders map[metadata]threecommas.MarketOrder) map[metadata]threecommas.MarketOrder {
+	txn := db.NewTransaction(false)
+	defer txn.Discard()
+
+	unprocessed := make(map[metadata]threecommas.MarketOrder)
+	for md, order := range orders {
+		// key := fmt.Sprintf("%d::%d::%s", botId, dealId, order.OrderId)
+		log.Printf("Looking for %s\n", md.String())
+		_, err := txn.Get([]byte(md.String()))
+
+		switch err {
+		case badger.ErrKeyNotFound:
+			// unprocessed = append(unprocessed, order)
+			unprocessed[md] = order
+			log.Printf("Unprocessed %s\n", md.String())
+
+		case nil:
+			continue
+
+		default:
+			log.Printf("Unexpected error for key %s: %s", md.String(), err)
+		}
+	}
+
+	return unprocessed
+}
+
+func unprocessedMarketOrders(db *badger.DB, botId, dealId int, orders []threecommas.MarketOrder) []threecommas.MarketOrder {
+	txn := db.NewTransaction(false)
+	defer txn.Discard()
+
+	var unprocessed []threecommas.MarketOrder
+	for _, order := range orders {
+		key := fmt.Sprintf("%d::%d::%s", botId, dealId, order.OrderId)
+		// log.Printf("Looking for %s\n", key)
+		_, err := txn.Get([]byte(key))
+
+		switch err {
+		case badger.ErrKeyNotFound:
+			unprocessed = append(unprocessed, order)
+
+		case nil:
+			continue
+
+		default:
+			log.Printf("Unexpected error for key %s: %s", key, err)
+		}
+	}
+
+	return unprocessed
+}

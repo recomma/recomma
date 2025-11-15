68142de6c917b2b1616429f2a74d03deeeef563f
Yorick Terweijden <hi@terwey.me>
2025-11-15 11:42:15 +0100

    chore: remove unnecessary import

--- cmd/recomma/e2e_comprehensive_test.go
+++ cmd/recomma/e2e_comprehensive_test.go
@@ -8,7 +8,6 @@ import (
 	threecommasmock "github.com/recomma/3commas-mock/server"
 	tc "github.com/recomma/3commas-sdk-go/threecommas"
 	"github.com/recomma/recomma/internal/api"
-	"github.com/recomma/recomma/storage"
 	"github.com/stretchr/testify/require"
 )
 
